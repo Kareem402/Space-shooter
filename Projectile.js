bb24c1fe03b84d9d076770806b22decbea644d54
Karem <karem.....@Karems-MacBook-Air.local>
2025-05-01 21:09:54 -0600

    Implement projectile behavior and rendering

---
+++ Projectile.js
@@ -0,0 +1,17 @@
+class Projectile {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.speed = 8;
+    }
+  
+    update() {
+      this.y -= this.speed;
+    }
+  
+    display() {
+      fill(255);
+      rect(this.x, this.y, 4, 10);
+    }
+  }
+  
